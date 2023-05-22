6487d44c48ffe73d2664f95d9f0c1412227b6341
1010nishant <nishantjangid6377@gmail.com>
2023-05-22 22:41:00 +0530

    added some data and img for future use and understand how to show static file on browser

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 //middleware
 app.use(express.json())
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`))
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
