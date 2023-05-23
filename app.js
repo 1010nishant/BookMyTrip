02e085ca58ce222ee7ae751a6824126e1580f771
1010nishant <nishantjangid6377@gmail.com>
2023-05-23 16:08:42 +0530

    done about dotenv but not eslint yet

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 //middleware
 app.use(express.json())
-app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/public`))
 
 const tourRouter = require('./routes/tourRoutes');
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './config.env' })
 const app = require('./app')
 
-const port = 3000;
+// console.log(process.env)
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`app is running on ${port}....`);
 })
