f5b8022a319d93a3ef8207fbd069d33adbbca085
1010nishant <nishantjangid6377@gmail.com>
2023-06-03 17:49:08 +0530

    fields limiting and pagination is done

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -26,7 +26,28 @@ exports.getAllTours = async (req, res) => {
             query = query.sort('-createAt')
 
         }
+        // 4) field limiting
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ')
+            console.log(fields)
+            query = query.select(fields)
+        } else {
+            query = query.select('-__v')
 
+        }
+
+        // 5) pagination
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit
+
+        // page=3&limit=10, 1-10 -> page=1, 11-20 -> page=2, 21-30 -> page=3
+        query = query.skip(skip).limit(limit) // --> page=3
+
+        if (req.query.page) {
+            const numTours = await Tour.countDocuments()
+            if (skip >= numTours) throw new Error('this page does not exist')
+        }
         const tours = await query
         res.status(200).json({
             status: 'success',
