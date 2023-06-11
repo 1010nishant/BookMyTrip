d37cf3de48b2494615edcdca1e23539ca2938041
1010nishant <nishantjangid6377@gmail.com>
2023-06-11 19:08:01 +0530

    virtual properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -49,9 +49,18 @@ const tourSchema = mongoose.Schema({
     images: [String],
     createAt: {
         type: Date,
-        default: Date.now()
+        default: Date.now(),
+        select: false
+
     },
     startDates: [Date]
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+})
+
+tourSchema.virtual('durationWeeks').get(function () {
+    return this.duration / 7
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
